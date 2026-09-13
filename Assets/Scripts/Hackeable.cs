62c749058102d69e8f6f947d8fe4aa22a66e4ec8
MaycolSantiagoR <Maycolsantiago2005@gmail.com>
2026-09-13 18:57:31 -0300

    modificaciones hack

--- Assets/Scripts/Hackeable.cs
+++ Assets/Scripts/Hackeable.cs
@@ -1,18 +1,28 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class Hackeable : MonoBehaviour
 {
     private Renderer objectRenderer;
+    private Color originalColor;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
       objectRenderer = GetComponent<Renderer>();  
+        originalColor = objectRenderer.material.color;
     }
 
    public void Hack()
     {
+        StartCoroutine(HackEffect());
+
+    }
+    private IEnumerator HackEffect()
+    {
         objectRenderer.material.color = Color.green;
+        yield return new WaitForSeconds(3f);
+        objectRenderer.material.color = originalColor;
     }
 }
